c4818625311150e3a68a963333e13368104eb205
Kamil <kkedzierski@apptension.com>
2022-07-13 19:46:12 +0200

    Added user in the submission details

--- src/screens/main/submissionDetails/submissionDetails.component.tsx
+++ src/screens/main/submissionDetails/submissionDetails.component.tsx
@@ -1,42 +1,42 @@
-import { faBolt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { doc, updateDoc } from "firebase/firestore";
-import { useDocumentData } from "react-firebase-hooks/firestore";
-import { useForm } from "react-hook-form";
-import { useNavigate, useParams } from "react-router-dom";
-import { BackNavigation } from "../../../components/backNavgation";
-import { DetailsItem } from "../../../components/detailsItem";
-import { Loader } from "../../../components/loader";
-import { ScreenHeader } from "../../../components/screenHeader";
-import { Status } from "../../../components/status";
-import { useUserContext } from "../../../hooks/useUser";
-import { db } from "../../../services/firebase";
-import { triggerToast } from "../../../utils/triggerToast";
+import { faBolt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { doc, updateDoc } from 'firebase/firestore';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useForm } from 'react-hook-form';
+import { useNavigate, useParams } from 'react-router-dom';
+import { BackNavigation } from '../../../components/backNavgation';
+import { DetailsItem } from '../../../components/detailsItem';
+import { Loader } from '../../../components/loader';
+import { ScreenHeader } from '../../../components/screenHeader';
+import { Status } from '../../../components/status';
+import { useUserContext } from '../../../hooks/useUser';
+import { db } from '../../../services/firebase';
+import { triggerToast } from '../../../utils/triggerToast';
 
 export const SubmissionDetailsComponent = () => {
   const { id } = useParams();
-  const submissionRef = doc(db, "submissions", id!);
+  const submissionRef = doc(db, 'submissions', id!);
 
   const { user } = useUserContext();
-  const [value, loading, error] = useDocumentData(doc(db, "submissions", id!));
+  const [value, loading, error] = useDocumentData(doc(db, 'submissions', id!));
 
   const navigate = useNavigate();
 
   const handleAccept = async () => {
     try {
       await updateDoc(submissionRef, {
-        status: "accepted",
+        status: 'accepted',
         acceptedOnDate: Date.now(),
         reviewer: user.userName,
       });
       triggerToast(
-        "The submission was accepted successfuly!",
-        "success",
+        'The submission was accepted successfuly!',
+        'success',
         <FontAwesomeIcon icon={faBolt} color="#83E933" />
       );
-      navigate("/submissions");
+      navigate('/submissions');
     } catch (e) {
-      triggerToast("Some error occurred", "error", false);
+      triggerToast('Some error occurred', 'error', false);
     }
   };
 
@@ -49,32 +49,29 @@ export const SubmissionDetailsComponent = () => {
   const onSubmit = async (data: any) => {
     try {
       await updateDoc(submissionRef, {
-        status: "rejected",
+        status: 'rejected',
         rejectionMessage: data.rejectMessage,
         rejectedOnDate: Date.now(),
         reviewer: user.userName,
       });
       triggerToast(
-        "The submission was rejected successfuly!",
-        "success",
+        'The submission was rejected successfuly!',
+        'success',
         <FontAwesomeIcon icon={faBolt} color="#83E933" />
       );
-      navigate("/submissions");
+      navigate('/submissions');
     } catch (e) {
-      triggerToast("Some error occurred", "error", false);
+      triggerToast('Some error occurred', 'error', false);
     }
   };
 
   if (loading) return <Loader />;
 
   const renderViewCondition = () => {
-    if (user?.role === "admin") {
+    if (user?.role === 'admin') {
       return (
         <>
-          <button
-            className="btn btn-primary w-full mb-5"
-            onClick={handleAccept}
-          >
+          <button className="btn btn-primary w-full mb-5" onClick={handleAccept}>
             Accept
           </button>
 
@@ -85,16 +82,14 @@ export const SubmissionDetailsComponent = () => {
           <input type="checkbox" id="my-modal-4" className="modal-toggle" />
           <label htmlFor="my-modal-4" className="modal cursor-pointer">
             <label className="modal-box relative flex flex-col " htmlFor="">
-              <h3 className="text-lg font-bold">
-                You're about to reject the submission!
-              </h3>
+              <h3 className="text-lg font-bold">You're about to reject the submission!</h3>
               <p>Please write an explanation why</p>
               <form onSubmit={handleSubmit(onSubmit)}>
                 <textarea
                   className="p-4 text-black mt-5 mb-5 resize-none w-full h-40"
                   placeholder=""
                   required
-                  {...register("rejectMessage", { required: true })}
+                  {...register('rejectMessage', { required: true })}
                 />
 
                 <button className="btn w-full" type="submit">
@@ -114,12 +109,7 @@ export const SubmissionDetailsComponent = () => {
         <div className="mb-3" />
         <DetailsItem label="Reviewed by" text={value?.reviewer} />
         <div className="mb-3" />
-        {value?.rejectionMessage && (
-          <DetailsItem
-            label="Rejection message"
-            text={value?.rejectionMessage}
-          />
-        )}
+        {value?.rejectionMessage && <DetailsItem label="Rejection message" text={value?.rejectionMessage} />}
       </>
     );
   };
@@ -129,10 +119,9 @@ export const SubmissionDetailsComponent = () => {
       <ScreenHeader title="Submission Details" />
       <DetailsItem label="Activity name" text={value?.name} />
       <DetailsItem label="Activity description" text={value?.description} />
+      <DetailsItem label="User" text={value?.userName} />
       <DetailsItem label="Solution" text={value?.solution} />
-      {value?.attachment && (
-        <DetailsItem label="Attachment" attachmentURL={value?.attachment} />
-      )}
+      {value?.attachment && <DetailsItem label="Attachment" attachmentURL={value?.attachment} />}
       {renderViewCondition()}
     </div>
   );
